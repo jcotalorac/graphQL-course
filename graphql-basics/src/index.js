f8f76911a0516ff7d6088ab9c49c3a2c87189a13
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-18 19:08:09 -0500

    In abscence, default is provided and can have a different name

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,7 @@
 import currentLocation, { message, name } from './myModule'
+import currLocation from './myModule';
 
 console.log(message);
 console.log(name);
 console.log(currentLocation);
+console.log(currLocation);
