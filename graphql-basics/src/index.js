9b10e422826f90c2b22a570c3346cd28fc6e6ed4
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-29 10:45:26 -0500

    Adding definition and resolver to query schema

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,12 +3,16 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
         type Query {
             hello: String!
+            name: String!
         }
 `
 const resolvers = {
     Query: {
         hello() {
             return 'This is my first query!'
+        },
+        name() {
+            return 'Name JC'
         }
     }
 }
