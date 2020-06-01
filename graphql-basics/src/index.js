3fe4e65253dcdee4f495523a589e01f6ebff8da5
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-01 17:01:32 -0500

    Cleaning examples

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,9 +2,7 @@ import { GraphQLServer } from 'graphql-yoga'
 
 const typeDefs = `
         type Query {
-            greeting(name: String): String!
-            add(numbers: [Float!]!): Float!
-            grades: [Int!]!
+            
             me: User!
             post: Post!
         }
@@ -25,24 +23,7 @@ const typeDefs = `
 `
 const resolvers = {
     Query: {
-        greeting(parent, args, ctx, info) {
-            console.log(args);
-            if(args.name) {
-                return `Hello ${args.name}`
-            }
-            return 'Hello!!'
-        },
-        add(parent, args, ctx, info) {
-            if(args.numbers.lenght === 0) {
-                return 0
-            }
-            return args.numbers.reduce((accumulator, currentValue) => {
-                return accumulator + currentValue
-            })
-        },
-        grades(parent, args, ctx, info) {
-            return [99, 80, 93]
-        },
+        
         me() {
             return {
                 id: 123098,
