4d7a5ef24dcb2db27a6bec91a0faa0dae1d09056
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-02 12:07:17 -0500

    Establishing relationship to get comments belonging to a post

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -94,6 +94,7 @@ const typeDefs = `
             body: String!
             published: Boolean!
             author: User!
+            comments: [Comment!]!
         }
 
         type Comment {
@@ -143,6 +144,9 @@ const resolvers = {
     Post: {
         author(parent, args, ctx, info) {
             return users.find((user) => user.id === parent.author)
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => comment.post === parent.id)
         }
     },
     User: {
