cfd352eb2eb97e20c4f9cffb686054e3356dcd05
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-03 17:52:39 -0500

    Assigning db to graphql server context

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,75 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { v4 as uuidv4 } from 'uuid'
-
-let users = [
-    {
-        id: '1',
-        name: 'First',
-        email: 'mail@mail',
-        age: 28
-    },
-    {
-        id: '2',
-        name: 'Second',
-        email: 'email@mail'
-    },
-    {
-        id: '3',
-        name: 'Third',
-        email: 'em@em'
-    }
-]
-
-let posts = [
-    {
-        id: '1',
-        title: 'Title1',
-        body: 'Body1',
-        published: true,
-        author: '1'
-    },
-    {
-        id: '2',
-        title: 'Title2',
-        body: 'Body2',
-        published: false,
-        author: '1'
-    },
-    {
-        id: '3',
-        title: 'Title3',
-        body: 'Body3',
-        published: false,
-        author: '2'
-    }
-]
-
-let comments = [
-    {
-        id: '1',
-        text: 'Comment1',
-        author: '1',
-        post: '1'
-    },
-    {
-        id: '2',
-        text: 'Comment2',
-        author: '2',
-        post: '2'
-    },
-    {
-        id: '3',
-        text: 'Comment3',
-        author: '3',
-        post: '3'
-    },
-    {
-        id: '4',
-        text: 'Comment4',
-        author: '1',
-        post: '1'
-    }
-]
+import db from './db'
 
 const resolvers = {
     Query: {
@@ -234,7 +165,10 @@ const resolvers = {
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: {
+        db
+    }
 })
 
 server.start(() => {
