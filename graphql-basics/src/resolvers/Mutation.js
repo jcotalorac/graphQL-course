04c7e4fc58c2a33ddc25e3ca80da2b857bee8f11
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-03 21:33:14 -0500

    Mutation to update post

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -93,6 +93,29 @@ const Mutation = {
 
         return deletedPosts[0]
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args
+
+        const post = db.posts.find((post) => post.id === id)
+
+        if(!post) {
+            throw new Error('Post does not exist')
+        }
+
+        if(typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if(typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if(typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        return post
+    },
     createComment(parent, args, { db }, info) {
         const authorExist = db.users.some((user) => user.id === args.data.author)
 
