83bb59747c4f79a7ca027cfa7504255296f04fbf
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-09 15:38:59 -0500

    Returning a named channel able to subscribing

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,7 +1,9 @@
 const Subscription = {
     count: {
         subscribe(parent, args, { pubsub }, info) {
+            let count = 0
 
+            return pubsub.asyncIterator('count')
         }
     }
 }
