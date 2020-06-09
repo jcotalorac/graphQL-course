048860ffb09815f25cb43a66de3e36bc83a5d5e9
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-09 15:49:04 -0500

    Setting a time interval to publish data in the channel

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -3,6 +3,13 @@ const Subscription = {
         subscribe(parent, args, { pubsub }, info) {
             let count = 0
 
+            setInterval(() => {
+                count++
+                pubsub.publish('count', {
+                    count
+                })
+            }, 1000)
+
             return pubsub.asyncIterator('count')
         }
     }
