dd1c78e04cfdaef1b32dfc61c472740a7cf6ca91
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-17 18:44:08 -0500

    Using async await notation to update post and query user author

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -35,7 +35,7 @@ const prisma = new Prisma({
 //     console.log(JSON.stringify(data, undefined, 2));
 // })
 
-prisma.mutation.updatePost({
+/* prisma.mutation.updatePost({
     data: {
         body: "Updating body",
         published: true
@@ -50,4 +50,37 @@ prisma.mutation.updatePost({
 })
 .then((data) => {
     console.log(JSON.stringify(data, undefined, 2))
+}) */
+
+const createPostForUser = async (authorId, data) => {
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ id }')
+
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, '{ id name email posts { id title published } }')
+
+    return user
+}
+
+createPostForUser('ckbd5f5if03hy0784dworkrig', {
+    title: 'Great books to read',
+    body: 'The War of Art',
+    published: true
+})
+.then((user) => {
+    console.log(JSON.stringify(user, undefined, 2));
+})
+.catch((error) => {
+    console.log(error);
 })
