dff6fd9d32c82c9dd3d36d51d68cadf22f207545
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-17 17:11:33 -0500

    Create post mutation from prisma binding

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,12 +5,28 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name email posts { id title } }')
-.then((data) => {
-    console.log(JSON.stringify(data, undefined, 2));
-})
+// prisma.query.users(null, '{ id name email posts { id title } }')
+// .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }')
+// prisma.query.comments(null, '{ id text author { id name } }')
+// .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// })
+
+prisma.mutation.createPost({
+    data: {
+        title: "My new GraphQL post is live!",
+        body: "You can find the new course here",
+        published: true,
+        author: {
+            connect: {
+                id: "ckbd5f5if03hy0784dworkrig"
+            }
+        }
+    }
+}, '{ id title body published }')
 .then((data) => {
-    console.log(JSON.stringify(data, undefined, 2));
+    console.log(data);
 })
