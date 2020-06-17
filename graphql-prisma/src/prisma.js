2d4be96b0f3dbeab7f37b23018849c6c9231de9d
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-17 17:01:38 -0500

    Comments query from prisma binding

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -9,3 +9,8 @@ prisma.query.users(null, '{ id name email posts { id title } }')
 .then((data) => {
     console.log(JSON.stringify(data, undefined, 2));
 })
+
+prisma.query.comments(null, '{ id text author { id name } }')
+.then((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+})
