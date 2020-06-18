3fc1531896645aab65d400b7711d7df0a4792e83
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-18 18:23:36 -0500

    Returning prisma users query from database

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,11 +1,15 @@
 const Query = {
     users(parent, args, { db, prisma }, info) {
-        if(!args.query) {
-            return db.users
-        }
+        
+        return prisma.query.users(null, info)
+        
+        // if(!args.query) {
+        //     return db.users
+        // }
 
-        return db.users.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase())
-        )
+        // return db.users.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase())
+        // )
+
     },
     posts(parent, args, { db }, info) {
         if(!args.query) {
