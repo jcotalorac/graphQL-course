182894de5209e2682cffa70719503f0876c954e6
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-28 20:22:45 -0500

    Login user mutation

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -29,6 +29,29 @@ const Mutation = {
             token: jwt.sign({ userId: user.id }, 'thisisasecret')
         }
     },
+    async loginUser(parent, args, { prisma }, info) {
+        
+        const user = await prisma.query.user({
+            where: {
+                email: args.data.email
+            }
+        })
+        
+        if(!user) {
+            throw new Error('Invalid credentials')
+        }
+
+        const isMatch = await bcrypt.compare(args.data.password, user.password)
+
+        if(!isMatch) {
+            throw new Error('Invalid credentials')
+        }
+
+        return {
+            user,
+            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+        }
+    },
     async deleteUser(parent, args, { prisma }, info) {
         await validateUserExistence(prisma, args)
 
