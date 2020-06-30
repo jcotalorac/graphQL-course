ed4398e0efcb84636963aa5e69f9f69abd4f2326
juan.otalora <juancarlos.otalora@wnco.com>
2020-06-30 10:36:07 -0500

    Securing user updating with authentication from token and use authenticated id

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -62,12 +62,12 @@ const Mutation = {
             }
         }, info)
     },
-    async updateUser(parent, args, { prisma }, info) {
-        await validateUserExistence(prisma, args)
-        
+    async updateUser(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+
         return prisma.mutation.updateUser({
             where: {
-                id: args.id
+                id: userId
             },
             data: args.data
         }, info)
