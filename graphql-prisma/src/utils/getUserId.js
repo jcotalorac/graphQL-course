4593d3675f49d79247559689a6619b3b11cc101b
juan.otalora <juancarlos.otalora@wnco.com>
2020-07-03 15:37:52 -0500

    Refactoring authentication structure to set optional authentication

--- graphql-prisma/src/utils/getUserId.js
+++ graphql-prisma/src/utils/getUserId.js
@@ -1,17 +1,17 @@
 import jwt from 'jsonwebtoken'
 
-const getUserId = (request) => {
+const getUserId = (request, requiredAuth = true) => {
     
     const headerAuth = request.request.headers.authorization
 
-    if(!headerAuth) {
-        throw new Error('Authentication required')
+    if(headerAuth) {
+        const token = headerAuth.replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisisasecret')
+    
+        return decoded.userId
     }
-
-    const token = headerAuth.replace('Bearer ', '')
-    const decoded = jwt.verify(token, 'thisisasecret')
-
-    return decoded.userId
+    
+    throw new Error('Authentication required')
 }
 
 export { getUserId as default }
