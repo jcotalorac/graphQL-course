a02f996dcbdaf310afcab5ed4714ac78d1a5cf80
juan.otalora <juancarlos.otalora@wnco.com>
2020-07-10 18:34:49 -0500

    Testing a user creation operation, and printing response. Babel polyfill is required to recognize async-await notation

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,8 +1,31 @@
 import 'cross-fetch/polyfill'
+import '@babel/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
 
 const client = new ApolloBoost({
     uri: "http://localhost:4000"
 })
 
-test('Hi', () => {})
+test('Should create a new user', async () => {
+    const createUser = gql`
+        mutation {
+            createUser (
+                data: {
+                    name: "user",
+                    email: "mail@mail.com",
+                    password: "12345678"
+                }
+            ) {
+                token
+                user {
+                    id
+                }
+            }
+        }
+    `
+
+    const response = await client.mutate({
+        mutation: createUser
+    })
+    console.log(response);
+})
