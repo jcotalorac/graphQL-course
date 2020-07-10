f15fbf37973291124bc392736e0ac46685981162
juan.otalora <juancarlos.otalora@wnco.com>
2020-07-10 15:27:50 -0500

    Starting server instance and referencing from global scope for handling

--- graphql-prisma/tests/jest/globalSetup.js
+++ graphql-prisma/tests/jest/globalSetup.js
@@ -2,4 +2,8 @@ require('babel-register')
 require('@babel/polyfill')
 const server = require('../../src/server').default
 
-module.exports = async () => {}
+module.exports = async () => {
+    global.httpServer = await server.start({ port: process.env.PORT || 4000 }, ({ port }) => {
+        console.log('The server is up on port ' + port + '!')
+    })
+}
