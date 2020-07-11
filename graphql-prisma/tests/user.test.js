0e64fd7daa25aa04e1794b0d63606dfab06d3b20
juan.otalora <juancarlos.otalora@wnco.com>
2020-07-10 22:52:44 -0500

    Creating posts associated to user

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -10,11 +10,37 @@ const client = new ApolloBoost({
 
 beforeEach(async () => {
     await prisma.mutation.deleteManyUsers()
-    await prisma.mutation.createUser({
+    const user = await prisma.mutation.createUser({
         data: {
             name: "name",
             email: "name@mail.com",
             password: bcrypt.hashSync("87654321")
+        }
+    })
+
+    await prisma.mutation.createPost({
+        data: {
+            title: "Published post",
+            body: "Body published",
+            published: true,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.mutation.createPost({
+        data: {
+            title: "Unpublished post",
+            body: "Body unpublished",
+            published: false,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
         }
     })
 })
