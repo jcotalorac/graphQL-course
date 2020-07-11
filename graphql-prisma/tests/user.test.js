8b30a3b78f71bb0e559ee52237e8f096be24f0e8
juan.otalora <juancarlos.otalora@wnco.com>
2020-07-10 23:16:04 -0500

    Test case to show only published posts

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -93,3 +93,23 @@ test('Should expose public author profiles', async () => {
     expect(response.data.users[0].email).toBe(null)
     expect(response.data.users[0].name).toBe('name')
 })
+
+test('Should expose published posts', async () => {
+    const getPosts = gql`
+        query {
+            posts {
+                id
+                title
+                body
+                published
+            }
+        }
+    `
+
+    const response = await client.query({
+        query: getPosts
+    })
+
+    expect(response.data.posts.length).toBe(1)
+    expect(response.data.posts[0].published).toBe(true)
+})
